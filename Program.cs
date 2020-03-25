0a5f158ea6544e4e3e313cb46bab47f30840df39
OskarLovsveden <ol222hf@student.lnu.se>
2020-03-25 23:42:21 +0100

    getting max value of data.json functional

--- Program.cs
+++ Program.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using System.IO;
+using Newtonsoft.Json;
 
 namespace examination_1
 {
+    /// <summary>
+    /// Represents the main place where the program starts the execution.
+    /// </summary>
     class Program
     {
+
+        /// <summary>
+        /// The starting point of the application.
+        /// </summary>
+        /// <param name="args">Array containing arguments passed to the application.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var reader = new StreamReader(args[0]))
+            {
+                string json = reader.ReadToEnd();
+                int[] array = JsonConvert.DeserializeObject<int[]>(json);
+
+                int max = Statistics.Maximum(array);
+                System.Console.WriteLine(max);
+            }
         }
     }
 }
--- Statistics.cs
+++ Statistics.cs
@@ -1,12 +1,21 @@
-using System.IO;
+using System.Linq;
 
 namespace examination_1
 {
-    class Statistics
+    /// <summary>
+    /// Represents a statistics class.
+    /// </summary>
+    public class Statistics
     {
+        /// <summary>
+        /// Gets the max value of an array containing numbers.
+        /// </summary>
+        /// <param name="source">The source array to get the max value of.</param>
+        /// <returns></returns>
         public static int Maximum(int[] source)
         {
-
+            int max = source.Max();
+            return max;
         }
     }
 }
