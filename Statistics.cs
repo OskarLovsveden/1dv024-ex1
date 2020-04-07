8108f29f8ca2263b02374e69e257cbb435710bff
ol222hf <ol222hf@student.lnu.se>
2020-04-07 20:34:00 +0200

    Adding Mode

--- Statistics.cs
+++ Statistics.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace examination_1
 {
@@ -67,6 +68,45 @@ namespace examination_1
                 double median = (copy[lowIndex] + copy[highIndex]) / 2;
                 return median;
             }
+        }
+
+        /// <summary>
+        /// Gets the most frequently occuring values in an array containing numbers.
+        /// </summary>
+        /// <param name="source">The source array to get the data of.</param>
+        /// <returns></returns>
+        public static int[] Mode(int[] source)
+        {
+            int[] copy = new int[source.Length];
+            source.CopyTo(copy, 0);
+            Array.Sort(copy);
+
+            Dictionary<int, int> numbers = new Dictionary<int, int>();
+            foreach (int number in copy)
+            {
+                if (numbers.ContainsKey(number))
+                {
+                    numbers[number]++;
+                }
+                else
+                {
+                    numbers.Add(number, 1);
+                }
+            }
+
+            List<KeyValuePair<int, int>> list = numbers.ToList();
+            int max = list.Max(number => number.Value);
+            List<int> modeList = new List<int>();
+
+            foreach (KeyValuePair<int, int> numberPair in list)
+            {
+                if (numberPair.Value == max)
+                {
+                    modeList.Add(numberPair.Key);
+                }
+            }
+
+            return (int[])modeList.ToArray();
         }
     }
 }
